d8995af412cb82cd9fcc20e0cd502a4437c57be7
Miad008  <10694@holbertonstudents.com>
2026-02-23 11:42:48 +0000

    Task 4: Line Up

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Module that adds two arrays element-wise"""
 
+
 def add_arrays(arr1, arr2):
     """adds two arrays element-wise"""
     if len(arr1) != len(arr2):
         return None
-    return [arr1[!] + arr2[i] for i in range(len(arr1))]
+    return [arr1[i] + arr2[i] for i in range(len(arr1))]
