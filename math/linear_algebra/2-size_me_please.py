83c8f081bfee024e32214a8d99721276790498fd
Miad008  <10694@holbertonstudents.com>
2026-02-23 10:22:11 +0000

    Task 2: Size Me Please

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+"""Module that calculates the shape of a matrix"""
 def matrix_shape(matrix):
+    """calculates the shape of a matrix"""
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
