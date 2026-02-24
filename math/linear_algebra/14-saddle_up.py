f9a8f9ceb0c2178d72c42c7aee1ec7966cb1ed3b
Miad008  <10694@holbertonstudents.com>
2026-02-24 12:13:21 +0000

    Implement matrix multiplication using numpy matmul

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""matrix multiplication using numpy"""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """returns matrix multiplication"""
+    return np.matmul(mat1, mat2)
