f066a86cf5f4470850e49d983eb8c64abd25012b
Miad008  <10694@holbertonstudents.com>
2026-03-07 20:42:30 +0000

    Add Normal class with z_score and x_value

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -7,13 +7,17 @@ class Normal:
 
     def __init__(self, data=None, mean=0., stddev=1.):
         """Initialize the normal distribution"""
+
         if data is None:
+
             if stddev <= 0:
                 raise ValueError("stddev must be a positive value")
+
             self.mean = float(mean)
             self.stddev = float(stddev)
 
         else:
+
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
 
@@ -23,4 +27,13 @@ class Normal:
             self.mean = sum(data) / len(data)
 
             variance = sum((x - self.mean) ** 2 for x in data) / len(data)
+
             self.stddev = variance ** 0.5
+
+    def z_score(self, x):
+        """Calculates the z-score of a given x-value"""
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """Calculates the x-value of a given z-score"""
+        return self.mean + z * self.stddev
