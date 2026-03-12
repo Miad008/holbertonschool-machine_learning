8e272bcc3e31bb335dde2e3cf5cfe5c4c839ad31
Miad008 <10694@holbertonstudents.com>
2026-03-12 22:16:22 +0300

    Create DataFrame from numpy array with alphabetical column labels

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+"""Creates a pandas DataFrame from a numpy ndarray"""
+
 import pandas as pd
-import string
+
 
 def from_numpy(array):
+    """Return a DataFrame with alphabetical column labels"""
     cols = array.shape[1]
-    labels = list(string.ascii_uppercase[:cols])
+    labels = [chr(65 + i) for i in range(cols)]
     return pd.DataFrame(array, columns=labels)
