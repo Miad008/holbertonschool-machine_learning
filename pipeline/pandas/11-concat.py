20333e7c8fa956983f78c68d937e112a6bc1035e
Miad008 <10694@holbertonstudents.com>
2026-04-12 20:22:53 +0300

    Solve task 11 concat
    Refactor concat function to use loc for filtering and clean up comments.

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python3
 """Module that concatenates two DataFrames."""
 
-
 import pandas as pd
+
+index = __import__('10-index').index
 
 
 def concat(df1, df2):
     """Concatenates df2 on top of df1 with proper indexing."""
-
-    # import index function
-    index = __import__('10-index').index
-
-    # set index
     df1 = index(df1)
     df2 = index(df2)
 
-    # filter df2
-    df2 = df2[df2.index <= 1417411920]
+    df2 = df2.loc[:1417411920]
 
-    # concatenate with labels
-    return pd.concat([df2, df1], keys=["bitstamp", "coinbase"])
+    return pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
