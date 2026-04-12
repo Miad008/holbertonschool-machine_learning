7c51257e82f99386071bf159672e78ab9a8f8298
Miad008 <10694@holbertonstudents.com>
2026-04-12 21:47:03 +0300

    Solve task 0 Line Graph

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
+"""A script that plots y as a solid red line."""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-def line():
 
+def line():
+    """ Plot y = x³ as a solid red line with x ranging from 0 to 10. """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-
-    x = np.arange(0, 11)
-    plt.plot(x, y, 'r-')
+    plt.plot(y, color='r', linestyle='-')
     plt.xlim(0, 10)
-
     plt.show()
