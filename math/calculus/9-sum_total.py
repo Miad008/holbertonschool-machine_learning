83785ad78d6f06971a5a3322ffc87c0c19ea5b4e
Miad008 <10694@holbertonstudents.com>
2026-04-12 21:07:25 +0300

    Solve task 9 summation_i_squared

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,7 +4,7 @@
 
 def summation_i_squared(n):
     """Returns the sum of squares from 1 to n"""
-    
+
     if type(n) is not int or n < 1:
         return None
     return (n * (n + 1) * (2 * n + 1)) // 6
