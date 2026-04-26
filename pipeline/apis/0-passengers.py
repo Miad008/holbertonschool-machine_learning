a3000855901bbfad58c2bba485df4b32d1f526ad
Miad008 <10694@holbertonstudents.com>
2026-04-26 20:07:17 +0300

    Solve task 0 passengers

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+"""Module for finding available Star Wars ships by passenger capacity."""
+
 import requests
 
 
 def availableShips(passengerCount):
+    """Return a list of ships that can hold passengerCount passengers."""
     url = "https://swapi-api.alx-tools.com/api/starships/"
     ships = []
 
@@ -10,15 +13,15 @@ def availableShips(passengerCount):
         res = requests.get(url)
         data = res.json()
 
-        for ship in data.get('results', []):
-            passengers = ship.get('passengers', '0')
+        for ship in data.get("results", []):
+            passengers = ship.get("passengers", "0")
 
-            if passengers not in ['unknown', 'n/a']:
-                passengers = passengers.replace(',', '')
+            if passengers not in ["unknown", "n/a"]:
+                passengers = passengers.replace(",", "")
 
                 if passengers.isdigit() and int(passengers) >= passengerCount:
-                    ships.append(ship.get('name'))
+                    ships.append(ship.get("name"))
 
-        url = data.get('next')
+        url = data.get("next")
 
     return ships
